0db4ce28b9f14936cda86f8bc6a0723174fc27bb
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-04-28 19:26:22 +0545

    feat(Core): add BaseModel Admin Class
    base model admin class was created

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+
+class BaseModelAdmin(ModelAdmin):
+    list_display = (
+        'id',
+        'updated_at',
+    )
+    list_display_links = (
+        'id',
+    )
+    search_fields = (
+        'id',
+    )
+    ordering = (
+        '-created_at',
+    )
+    list_per_page = (
+        25
+    )
