2684f9a2d45e63ec92ccbc96d33406cff06642cd
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-04-29 17:30:16 +0545

    feat(Project): Include Book Url to project

--- olenepal_book_management/urls.py
+++ olenepal_book_management/urls.py
@@ -33,8 +33,28 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/user/', include('apps.users.urls')),
-    path('api/docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(
+        'admin/', admin.site.urls
+    ),
+    path(
+        'api/v1/user/',
+        include('apps.users.urls')
+    ),
+
+    path(
+        'api/v1/books/',
+        include('apps.books.urls.base_urls')
+    ),
+
+    path(
+        'api/docs',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
