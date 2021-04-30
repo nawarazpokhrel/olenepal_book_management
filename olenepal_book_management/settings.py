c1bd2e90a84116816d62566d29eb688352a3f5ab
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-04-30 20:06:21 +0545

    feat(Projects): Add Permission class
    Permission class was added in setting

--- olenepal_book_management/settings.py
+++ olenepal_book_management/settings.py
@@ -103,9 +103,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',),
 
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework_simplejwt.permissions.IsAuthenticated',
-    # )
+    "DEFAULT_PERMISSION_CLASSES": (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 # Password validation
