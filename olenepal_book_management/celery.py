3e862545c948be9ab3fdd1b7996955fb3a73279b
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-05-01 23:25:19 +0545

    fix(project): upgrade requirements.txt

--- olenepal_book_management/celery.py
+++ olenepal_book_management/celery.py
@@ -11,12 +11,12 @@ app = Celery('olenepal_book_management')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+#
+# # Load task modules from all registered Django app configs.
+# app.autodiscover_tasks()
+#
+#
+# @app.task(bind=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
